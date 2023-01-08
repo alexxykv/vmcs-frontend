76528e7c79ea554850950dc2541d9fa2166390f9
Alexander Konovalov <alexxonov@gmail.com>
2023-01-08 17:02:29 +0500

    Create https server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
+
+const key = fs.readFileSync('./certs/private_key.pem', 'utf-8');
+const cert = fs.readFileSync('./certs/certificate_full_chain.pem', 'utf-8');
+
+const credentials = {
+  key: key,
+  cert: cert
+};
+
+const PORT = 443;
+const BUILD_FOLDER = 'build';
+
 const express = require('express');
 const app = express();
-
-const PORT = 3000;
-const BUILD_FOLDER = 'build';
 
 app.use(express.static(path.join(__dirname, BUILD_FOLDER)));
 
@@ -11,6 +22,8 @@ app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, BUILD_FOLDER, 'index.html'));
 });
 
-app.listen(PORT, () => {
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 });
