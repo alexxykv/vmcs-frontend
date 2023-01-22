2a560dc057308cbbae833a7992851125af5afc02
Alexander Konovalov <alexxonov@gmail.com>
2023-01-22 18:09:49 +0500

    Add Directories api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import Auth from './Auth';
 import ChannelInvitations from './ChannelInvitations';
 import Channels from './Channels';
+import Directories from './Directories';
 import Meetings from './Meetings';
 import Users from './Users';
 
@@ -8,6 +9,7 @@ export {
   Auth,
   ChannelInvitations,
   Channels,
+  Directories,
   Meetings,
   Users
 }
